9169d2966344d6afc8699b75c725a26dca83b390
zisuu <david.ziswiler@finecloud.ch>
2023-07-28 20:14:18 +0200

    wip controller tests

--- src/main/java/ch/finecloud/babytracker/config/TestConfig.java
+++ src/main/java/ch/finecloud/babytracker/config/TestConfig.java
@@ -12,7 +12,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Configuration
+//@Configuration
 public class TestConfig {
 
 //    @Bean
