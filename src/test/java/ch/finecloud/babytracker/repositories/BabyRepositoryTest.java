a541a937f04737f0ff29ca6c86096ba6387b7993
zisuu <david.ziswiler@finecloud.ch>
2023-08-14 19:53:00 +0200

    feature: repository tests

--- src/test/java/ch/finecloud/babytracker/repositories/BabyRepositoryTest.java
+++ src/test/java/ch/finecloud/babytracker/repositories/BabyRepositoryTest.java
@@ -1,59 +1,87 @@
-//package ch.finecloud.babytracker.repositories;
-//
-//import ch.finecloud.babytracker.bootstrap.BootstrapData;
-//import ch.finecloud.babytracker.entities.Baby;
-//import ch.finecloud.babytracker.entities.Event;
-//import ch.finecloud.babytracker.entities.UserAccount;
-//import ch.finecloud.babytracker.services.UserCsvServiceImpl;
-//import jakarta.transaction.Transactional;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-//import org.springframework.context.annotation.Import;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//@DataJpaTest
-//@Import({BootstrapData.class, UserCsvServiceImpl.class})
-//class BabyRepositoryTest {
-//
-//    @Autowired
-//    BabyRepository babyRepository;
-//
-//    @Autowired
-//    EventRepository eventRepository;
-//
-//    @Autowired
-//    UserAccountRepository userAccountRepository;
-//
-//    Event testEvent;
-//    UserAccount testUserAccount;
-//
-//    @BeforeEach
-//    void setUp() {
-//        testEvent = eventRepository.findAll().get(0);
-//        testUserAccount = userAccountRepository.findAll().get(0);
-//    }
-//
-//    @Transactional
-//    @Test
-//    void testBabyAccount() {
-//        Baby baby = Baby.builder()
-//                .name("testBaby")
-//                .build();
-//        baby.setUserAccount(testUserAccount);
-//        Baby savedBaby = babyRepository.save(baby);
-//
-//        assertNotNull(savedBaby);
-//        assertNotNull(savedBaby.getId());
-//
-//        Baby fetchedBaby = babyRepository.getById(savedBaby.getId());
-//
-//        assertNotNull(fetchedBaby);
-//        assertNotNull(fetchedBaby.getId());
-//        assertNotNull(fetchedBaby.getUserAccount());
-//    }
-//
-//}
+package ch.finecloud.babytracker.repositories;
+
+import ch.finecloud.babytracker.bootstrap.BootstrapData;
+import ch.finecloud.babytracker.config.TestConfig;
+import ch.finecloud.babytracker.entities.Baby;
+import ch.finecloud.babytracker.entities.Event;
+import ch.finecloud.babytracker.entities.UserAccount;
+import ch.finecloud.babytracker.services.UserCsvServiceImpl;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@Import({BootstrapData.class, UserCsvServiceImpl.class, TestConfig.class})
+class BabyRepositoryTest {
+
+    @Autowired
+    BabyRepository babyRepository;
+
+    @Autowired
+    EventRepository eventRepository;
+
+    @Autowired
+    UserAccountRepository userAccountRepository;
+
+    Event testEvent;
+    UserAccount testUserAccount;
+
+    @BeforeEach
+    void setUp() {
+        testEvent = eventRepository.findAll().get(0);
+        testUserAccount = userAccountRepository.findAll().get(0);
+    }
+
+    @Transactional
+    @Test
+    void testAddBaby() {
+        Baby baby = Baby.builder()
+                .name("testBaby")
+                .build();
+        baby.setUserAccount(testUserAccount);
+        Baby savedBaby = babyRepository.save(baby);
+        babyRepository.flush();
+
+        assertNotNull(savedBaby);
+        assertNotNull(savedBaby.getId());
+
+        Baby fetchedBaby = babyRepository.getById(savedBaby.getId());
+
+        assertNotNull(fetchedBaby);
+        assertNotNull(fetchedBaby.getId());
+        assertNotNull(fetchedBaby.getUserAccount());
+    }
+
+    @Transactional
+    @Test
+    void testDeleteBaby() {
+        Baby baby = Baby.builder()
+                .name("testBaby")
+                .build();
+        baby.setUserAccount(testUserAccount);
+        Baby savedBaby = babyRepository.save(baby);
+        babyRepository.flush();
+
+        assertNotNull(savedBaby);
+        assertNotNull(savedBaby.getId());
+
+        Baby fetchedBaby = babyRepository.findById(savedBaby.getId()).get();
+
+        assertNotNull(fetchedBaby);
+        assertNotNull(fetchedBaby.getId());
+        assertNotNull(fetchedBaby.getUserAccount());
+
+        babyRepository.delete(fetchedBaby);
+        babyRepository.flush();
+        Baby deletedBaby = babyRepository.findById(savedBaby.getId()).orElse(null);
+        assertNull(deletedBaby);
+    }
+
+}
--- src/test/java/ch/finecloud/babytracker/repositories/EventRepositoryTest.java
+++ src/test/java/ch/finecloud/babytracker/repositories/EventRepositoryTest.java
@@ -1,58 +1,84 @@
-//package ch.finecloud.babytracker.repositories;
-//
-//import ch.finecloud.babytracker.bootstrap.BootstrapData;
-//import ch.finecloud.babytracker.entities.Baby;
-//import ch.finecloud.babytracker.entities.Event;
-//import ch.finecloud.babytracker.model.EventType;
-//import ch.finecloud.babytracker.services.UserCsvServiceImpl;
-//import jakarta.transaction.Transactional;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-//import org.springframework.context.annotation.Import;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//@DataJpaTest
-//@Import({BootstrapData.class, UserCsvServiceImpl.class})
-//class EventRepositoryTest {
-//
-//    @Autowired
-//    BabyRepository babyRepository;
-//
-//    @Autowired
-//    EventRepository eventRepository;
-//
-//    @Autowired
-//    UserAccountRepository userAccountRepository;
-//
-//
-//    Baby testBaby;
-//
-//    @BeforeEach
-//    void setUp() {
-//        testBaby = babyRepository.findAll().get(0);
-//    }
-//
-//    @Transactional
-//    @Test
-//    void testBabyEvent() {
-//        Event event = Event.builder()
-//                .eventType(EventType.SLEEPING)
-//                .build();
-//        event.setBaby(testBaby);
-//        Event savedEvent = eventRepository.save(event);
-//
-//        assertNotNull(savedEvent);
-//        assertNotNull(savedEvent.getId());
-//
-//        Event fetchedEvent = eventRepository.getById(savedEvent.getId());
-//
-//        assertNotNull(fetchedEvent);
-//        assertNotNull(fetchedEvent.getId());
-//        assertNotNull(fetchedEvent.getBaby());
-//    }
-//
-//}
+package ch.finecloud.babytracker.repositories;
+
+import ch.finecloud.babytracker.bootstrap.BootstrapData;
+import ch.finecloud.babytracker.config.TestConfig;
+import ch.finecloud.babytracker.entities.Baby;
+import ch.finecloud.babytracker.entities.Event;
+import ch.finecloud.babytracker.model.EventType;
+import ch.finecloud.babytracker.services.UserCsvServiceImpl;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@Import({BootstrapData.class, UserCsvServiceImpl.class, TestConfig.class})
+class EventRepositoryTest {
+
+    @Autowired
+    BabyRepository babyRepository;
+
+    @Autowired
+    EventRepository eventRepository;
+
+    @Autowired
+    UserAccountRepository userAccountRepository;
+
+
+    Baby testBaby;
+
+    @BeforeEach
+    void setUp() {
+        testBaby = babyRepository.findAll().get(0);
+    }
+
+    @Transactional
+    @Test
+    void testAddBabyAddEvent() {
+        Event event = Event.builder()
+                .eventType(EventType.SLEEPING)
+                .build();
+        event.setBaby(testBaby);
+        Event savedEvent = eventRepository.save(event);
+        eventRepository.flush();
+
+        assertNotNull(savedEvent);
+        assertNotNull(savedEvent.getId());
+
+        Event fetchedEvent = eventRepository.getById(savedEvent.getId());
+
+        assertNotNull(fetchedEvent);
+        assertNotNull(fetchedEvent.getId());
+        assertNotNull(fetchedEvent.getBaby());
+    }
+
+    @Transactional
+    @Test
+    void testAddBabyAddEventDeleteEvent() {
+        Event event = Event.builder()
+                .eventType(EventType.SLEEPING)
+                .build();
+        event.setBaby(testBaby);
+        Event savedEvent = eventRepository.save(event);
+        eventRepository.flush();
+
+        assertNotNull(savedEvent);
+        assertNotNull(savedEvent.getId());
+
+        Event fetchedEvent = eventRepository.getById(savedEvent.getId());
+
+        assertNotNull(fetchedEvent);
+        assertNotNull(fetchedEvent.getId());
+        assertNotNull(fetchedEvent.getBaby());
+
+        eventRepository.delete(fetchedEvent);
+        eventRepository.flush();
+        assertThat(babyRepository.findById(testBaby.getId()).get().getEvents()).isEmpty();
+    }
+
+}
