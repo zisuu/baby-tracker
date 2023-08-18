88f3eb45edcd02b3bf8f83252f33015acebf9589
zisuu <david.ziswiler@finecloud.ch>
2023-08-18 14:41:04 +0200

    feat: frontend logout page

--- src/main/resources/static/js/components/logout.js
+++ src/main/resources/static/js/components/logout.js
@@ -5,6 +5,7 @@ export default {
 	title: 'Logout',
 	templatePath: 'logout.html',
 	requiresAuth: true,
+	css: 'signin.css',
 	init: function(view) {
 		store.clear();
 		util.showAuthContent(false);
